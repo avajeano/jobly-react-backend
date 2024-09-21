fb94ed0cfa38e6087aeaff455b189cbbad16b435
Ava Isaacson <avajeano@gmail.com>
2024-09-21 15:45:59 -0400

    DATABASE_URL update

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const PORT = +process.env.PORT || 3001;
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
       ? "jobly_test"
-      : process.env.DATABASE_URL || "postgresql://postgres.klqphzaynzccbhidnbco:7EN5edT49TzpF_J@aws-0-us-west-1.pooler.supabase.com:6543/postgres";
+      : process.env.DATABASE_URL;
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
