ec784c3e9342414422bba9874c35a0b5f70af3f0
Ava Isaacson <avajeano@gmail.com>
2024-09-21 21:52:26 -0400

    frontend url updated

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const morgan = require("morgan");
 const app = express();
 
 // frontend url
-app.use(cors({origin: "http://localhost:3000"}));
+app.use(cors({origin: "https://jobly-react-frontend.onrender.com"}));
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(authenticateJWT);
